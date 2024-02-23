3dc60cad0e527b4f272de4836a324829aecbbc52
ElaRCamo <112036581+ElaRCamo@users.noreply.github.com>
2024-02-23 16:49:56 -0600

    commitEla

--- python/ecal_interfaces.py
+++ python/ecal_interfaces.py
@@ -17,7 +17,7 @@ class OutputInterface:
 
     @staticmethod
     def getProto(topicName):
-        Proto = importlib.import_module("proto." + topicName + "_pb2")
+        Proto = importlib.import_module(topicName + "_pb2")
         return eval("Proto." + topicName)
 
     def startPublisher(self, topicName):
