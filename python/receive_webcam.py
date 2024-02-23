44a223087e85423124584bfd2cfdb12158623b56
GMrlsN <gabrielmn24@outlook.com>
2024-02-22 23:35:55 -0600

    InputInterface & ImageInput
    Implemented the InputInterface class and ImageInput for eCAL, this includes methods for receiving image data, extracting properties, and obtaining face detection information.

--- python/receive_webcam.py
+++ python/receive_webcam.py
@@ -1,25 +1,17 @@
-import sys
 import cv2 as cv
-import ecal.core.core as ecal_core
-import numpy as np
-from ecal.core.subscriber import ProtoSubscriber
-import proto.image_pb2 as imagen_pb2
+import ecal_interfaces as ecalio  # Assuming the classes are in ecal_interfaces.py
 
-ecal_core.initialize(sys.argv, "Python Protobuf Subscriber")
-sub = ProtoSubscriber("image", imagen_pb2.image)
-protobuf_message = imagen_pb2.image()
 
-while ecal_core.ok():
-    is_received, protobuf_message, time = sub.receive(1)
-    if is_received:
-        img = np.frombuffer(protobuf_message.data, dtype=np.uint8)
-        # uncompressed
-        # img = np.reshape(img, (protobuf_message.height, protobuf_message.width, protobuf_message.channels))
+# Instantiate the ImageInput class
+image_input = ecalio.ImageInput("image")
 
-        # jpg
-        img = cv.imdecode(img, cv.IMREAD_COLOR)
+while True:
+    print(len(str(image_input.getFaceDetection())))
 
+    img = image_input.receive_data()
+    if img is not None:
+        # Display the image
         cv.imshow('my webcam', img)
         if cv.waitKey(1) == 27:
-            break  # esc to quit
-ecal_core.finalize()
+            break  # esc to quit 
+
