59f6c7f8224d1c66fd446d972e437656ec90465e
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-03-01 12:24:17 -0600

    adding ecal output interface C++

---
+++ cpp/ecal_interface.h
@@ -0,0 +1,91 @@
+#ifndef ECALCPP_ECAL_INTERFACE_H
+#define ECALCPP_ECAL_INTERFACE_H
+
+#include <ecal/ecal.h>
+#include <ecal/msg/protobuf/publisher.h>
+#include <iostream>
+#include <string>
+#include "image.pb.h"
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+
+namespace eCALIO{
+    template<typename T_topic>
+    class OutputInterface {
+    private:
+        const char* processName;
+        const char* topicName;
+        bool messageWasSent;
+        eCAL::protobuf::CPublisher<T_topic> publisher;
+
+        void startProcess() {
+            eCAL::Initialize(0, nullptr, processName);
+            eCAL::Process::SetState(proc_sev_healthy, proc_sev_level1, " ");
+        }
+
+        void createPublisher() {
+            publisher.Create(getTopicName());
+        }
+
+    public:
+        T_topic message;
+
+        explicit OutputInterface(const char* topic = "ecal_output")
+                : topicName{topic} {
+            processName = "C++ Publisher";
+            messageWasSent = false;
+            startProcess();
+            createPublisher();
+        }
+
+        void sendMessage() {
+            setMessageWasSent(publisher.Send(message));
+        }
+
+        std::string getProcessName() const {
+            return processName;
+        }
+
+        std::string getTopicName() const {
+            return topicName;
+        }
+
+        bool getMessageWasSent() const {
+            return messageWasSent;
+        }
+
+        void setMessageWasSent(bool value) {
+            messageWasSent = value;
+        }
+    };
+
+    class ImageOutput : public OutputInterface<pb::image> {
+
+    public:
+        ImageOutput()
+                : OutputInterface("image") {}
+
+        void updateMessage(const cv::Mat image, pb::compression compression, pb::colorspace color) {
+
+            cv::uint8_t vectorizedImage[640 * 480 * 3];
+            int idx = 0;
+            for(int col=0; col<image.cols; col++){
+                for(int row=0; row<image.rows; row++){
+                    vectorizedImage[idx++] = image.at<cv::Vec3b>(row,col)[0];
+                    vectorizedImage[idx++] = image.at<cv::Vec3b>(row,col)[1];
+                    vectorizedImage[idx++] = image.at<cv::Vec3b>(row,col)[2];
+                }
+            }
+            message.set_height(image.cols);
+            message.set_width(image.rows);
+            message.set_imagecompression(pb::UNCOMPRESSED);
+            message.set_color(pb::RGB);
+            message.set_data(&vectorizedImage, image.rows * image.cols * 3);
+        }
+    };
+
+} // namespace eCALIO
+
+#endif //ECALCPP_ECAL_INTERFACE_H
--- cpp/send_webcam.cpp
+++ cpp/send_webcam.cpp
@@ -1,25 +1,10 @@
-#include <opencv2/core.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <ecal/ecal.h>
-#include <ecal/msg/protobuf/publisher.h>
-#include "image.pb.h"
+#include "ecal_interface.h"
 
 
 int main(int argc, char *argv[]) {
 
     // Subscribers names
-    const char *topic_mi_mensaje = "webcam_data";
-
-    // Creating Process
-    eCAL::Initialize(argc, argv, "C++ webcam Publisher");
-    eCAL::Process::SetState(proc_sev_healthy, proc_sev_level1, " ");
-    // Creating subscribers and its message
-    eCAL::protobuf::CPublisher<pb::image> pub_mensaje(topic_mi_mensaje);
-    pb::image protobuf_message;
-
+    eCALIO::ImageOutput imagePublisher;
     eCAL::Process::SleepMS(1000);
 
     cv::Mat frame, frame_gray;
@@ -42,23 +27,9 @@ int main(int argc, char *argv[]) {
 
         cv::imshow("Live", frame);
 
-        // Updating protobuf message
-        cv::uint8_t image[640 * 480 * 3];
-        int idx = 0;
-        for(int col=0; col<frame.cols; col++){
-            for(int row=0; row<frame.rows; row++){
-                image[idx++] = frame.at<cv::Vec3b>(row,col)[0];
-                image[idx++] = frame.at<cv::Vec3b>(row,col)[1];
-                image[idx++] = frame.at<cv::Vec3b>(row,col)[2];
-            }
-        }
-
-        protobuf_message.set_height(frame.cols);
-        protobuf_message.set_width(frame.rows);
-        protobuf_message.set_imagecompression(pb::UNCOMPRESSED);
-        protobuf_message.set_color(pb::RGB);
-        protobuf_message.set_data(&image, frame.rows * frame.cols * 3);
-        pub_mensaje.Send(protobuf_message);
+        // Updating & send protobuf message
+        imagePublisher.updateMessage(frame, pb::UNCOMPRESSED, pb::RGB);
+        imagePublisher.sendMessage();
         eCAL::Process::SleepMS(5);
 
         if (cv::waitKey(5) >= 0)
