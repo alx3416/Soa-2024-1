c75c7084ed3e5d7eda5004c6f120455a744e9c22
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-05-22 16:02:18 -0600

    Update task_rabbit.py

--- MQ/task_rabbit.py
+++ MQ/task_rabbit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-
+import time
 import pika
 
 connection = pika.BlockingConnection(
@@ -10,12 +10,16 @@ channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
 
 message = ' '.join(sys.argv[1:]) or "Hello World!"
-channel.basic_publish(
-    exchange='',
-    routing_key='task_queue',
-    body=message,
-    properties=pika.BasicProperties(
-        delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE,
-    ))
-print(f" [x] Sent {message}")
+count = 1
+while True:
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=message + str(count),
+        properties=pika.BasicProperties(
+            delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE,
+        ))
+    print(f" [x] Sent {message}" + str(count))
+    count += 1
+    time.sleep(1)
 connection.close()
