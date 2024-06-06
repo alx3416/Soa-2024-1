c913f0479f14b8b594345f552a59ff5cd0916ea4
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-06-06 01:29:18 -0600

    adding detection yolo segm

--- python/detect_show yolo_segm.py
+++ python/detect_show yolo_segm.py
@@ -1,11 +1,20 @@
-import cv2
 from ultralytics import YOLO
+import sys
+import cv2 as cv
+import ecal.core.core as ecal_core
+import numpy as np
+from ecal.core.subscriber import ProtoSubscriber
+import python.image_pb2 as imagen_pb2
+
+ecal_core.initialize(sys.argv, "Python Protobuf Subscriber")
+sub = ProtoSubscriber("image", imagen_pb2.imagen)
+protobuf_message = imagen_pb2
 
 # Load the model
 yolo = YOLO('yolov8s-seg.pt')
 
 # Load the video capture
-videoCap = cv2.VideoCapture(0)
+videoCap = cv.VideoCapture(0)
 
 while True:
     ret, frame = videoCap.read()
@@ -15,14 +24,14 @@ while True:
             frame = result.plot()
 
             # show the image
-        cv2.imshow('frame', frame)
+        cv.imshow('frame', frame)
 
     # break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
 
 # release the video capture and destroy all windows
 videoCap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
